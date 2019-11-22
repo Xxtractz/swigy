392bc1aaa44c44a76547c2eefc4f679c0e4f12b4
xxtractz <musambaloyi@gmail.com>
2019-11-22 16:21:20 +0200

    GUI

--- src/main/java/swingy/view/GUI.java
+++ src/main/java/swingy/view/GUI.java
@@ -3,7 +3,10 @@ import swingy.model.GameData;
 import swingy.model.Map;
 import swingy.model.Player;
 import swingy.model.Save;
+import swingy.model.hero.BlackPanther;
 import swingy.model.hero.Hero;
+import swingy.model.hero.IronMan;
+import swingy.model.hero.Thor;
 import swingy.model.villain.Erik;
 import swingy.model.villain.Loki;
 import swingy.model.villain.Thanos;
@@ -11,6 +14,10 @@ import swingy.model.villain.Villain;
 import swingy.utils_swingy.SwingyUtils;
 
 import javax.swing.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.awt.*;
+import java.util.Random;
 
 public class GUI implements IDisplay {
     private GameData gameData;
@@ -22,6 +29,7 @@ public class GUI implements IDisplay {
     private Villain loki;
     private Villain erik;
     private Save save;
+    private JFrame frame;
 
     public GUI(){
         gameData = new GameData();
@@ -32,29 +40,44 @@ public class GUI implements IDisplay {
         thanos = new Thanos();
         loki = new Loki();
         save = new Save();
+        frame = new JFrame();
     }
     @Override
     public void initGame() {
 
-        JOptionPane.showMessageDialog(null,"WElCOME TO Swingy".toUpperCase()+"\n"+gameData.gameInstructions());
+        JOptionPane.showMessageDialog(null,"$$$$$$$$$ Swingy $$$$$$$$$$$".toUpperCase()+"\n"+gameData.gameInstructions());
         getUser();
-//        JFrame header =  new JFrame();
-//
-//        header.add(gameData.gameBeginHeader());
-//
-//        header.setSize(800,800);
-//        header.setLayout(null);
-//        header.setVisible(true);
+        getHero();
+        beforePlay();
     }
 
     @Override
     public void getUser() {
-
+        @NotNull
+        @NotEmpty
+        String _player = JOptionPane.showInputDialog("Hi, Welcome to Swingy, Please enter your Name");
+        if (!_player.isEmpty()){
+            player.setPlayerName(_player);
+        }
+        else {
+            getUser();
+        }
     }
 
     @Override
     public void getHero() {
-
+        @NotNull
+        @NotEmpty
+        String selectHeroOpt = JOptionPane.showInputDialog(player.getPlayerName()+" !, Please Select Below, to Proceed\n" +
+                "1. Create a New Hero\n" +
+                "2. Select previous Hero");
+        if (selectHeroOpt.equals("1")){
+            createHero();
+        }
+        else if (selectHeroOpt.equals("2")) {
+            selectHero();
+        }
+        else getHero();
     }
 
     @Override
@@ -62,9 +85,67 @@ public class GUI implements IDisplay {
 
     }
 
+    private void beforePlay(){
+        map.setSize(hero.heroLevel());
+        hero.setCo_x(map.getSize()/2);
+        hero.setCo_y(map.getSize()/2);
+
+        setThanosLocation();
+        setLokiLocation();
+        setErikLocation();
+    }
+
+    private void setThanosLocation(){
+        int x = new Random().nextInt(map.getSize());
+        int y = new Random().nextInt(map.getSize());
+        if (x == map.getSize()/2 && y == map.getSize()/2){
+            setThanosLocation();
+        }else {
+            thanos.setVillain_X_Cor(x);
+            thanos.setVillain_Y_Cor(y);
+        }
+    }
+
+    private void setLokiLocation(){
+        int x = new Random().nextInt(map.getSize());
+        int y = new Random().nextInt(map.getSize());
+        if (x == map.getSize()/2 && y == map.getSize()/2){
+            setLokiLocation();
+        }else {
+            loki.setVillain_X_Cor(x);
+            loki.setVillain_Y_Cor(y);
+        }
+    }
+
+    private void setErikLocation(){
+        int x = new Random().nextInt(map.getSize());
+        int y = new Random().nextInt(map.getSize());
+        if (x == map.getSize()/2 && y == map.getSize()/2){
+            setErikLocation();
+        }else {
+            erik.setVillain_X_Cor(x);
+            erik.setVillain_Y_Cor(y);
+        }
+    }
+
     @Override
     public void createHero() {
-
+        String _heroType = JOptionPane.showInputDialog("Lets Create your Hero.!!\n" +
+                "Please Choose Hero Type\\n\" +\n" +
+                "   1. Thor\n" +
+                "   2. Iron Man\n" +
+                "   3. Black Panther");
+        if (_heroType.equals("1")){
+            hero = new Thor();
+        }
+        else if (_heroType.equals("2")){
+            hero = new IronMan();
+        }
+        else if (_heroType.equals("3")){
+            hero = new BlackPanther();
+        }else {
+            createHero();
+        }
     }
 
     @Override
@@ -75,6 +156,10 @@ public class GUI implements IDisplay {
     @Override
     public void playGame() {
 
+        frame.setLayout(new GridLayout());
+        frame.setSize(1080,800);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 }
